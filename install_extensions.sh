a9fa5beb39c1dd151c6fe11566b9b9e26ad534f5
Ofir Gal <ofirgal753@gmail.com>
2024-10-04 17:00:03 +0300

    install extensions is verbosed

--- install_extensions.sh
+++ install_extensions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-for i in $(cat extensions); do code --install-extension $i --force; done
+for i in $(cat extensions); do echo "Installing $i"; code --install-extension $i --force; done
